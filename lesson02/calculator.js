e1329d53f0da883aaa5f532f5f9a6bb67c470487
NayelaAhmed <nayelaa7@gmail.com>
2020-10-17 11:08:26 -0700

    finished assignment

---
+++ lesson02/calculator.js
@@ -0,0 +1,20 @@
+let x = 6;
+if(x >= 7){
+  console.log('hello');
+}
+
+const name = 36;
+const nayela = 7;
+let result = name / x;
+console.log(result);
+result = name - nayela;
+console.log(result);
+result = result + 1;
+console.log(result);
+result++;
+console.log(result);
+result += 1;
+console.log(result);
+
+
+
